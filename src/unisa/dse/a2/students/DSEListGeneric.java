f8917ae4e4ac174403051ab1d04c5a70504a9d78
girmy008 <164983725+girmy008@users.noreply.github.com>
2025-05-30 18:23:53 +0930

    porting remainder of methods into DSEListGeneric file
    porting remainder of methods into DSEListGeneric file

--- src/unisa/dse/a2/students/DSEListGeneric.java
+++ src/unisa/dse/a2/students/DSEListGeneric.java
@@ -19,26 +19,87 @@ public class DSEListGeneric implements ListGeneric {
 	
 	//Takes a list then adds each element into a new list
 	public DSEListGeneric(DSEListGeneric other) { // Copy constructor. 
-		this.head = other.head;
-		NodeGeneric currNode = other.head;
+		NodeGeneric headClone = new NodeGeneric(null,null,other.head.get());
+		this.head = headClone;
 		
-		while (currNode != null) {
-			NodeGeneric nodemaker = new NodeGeneric(currNode.next, currNode.prev, currNode.get());
-			currNode = currNode.next;
+		NodeGeneric ogIterator = other.head;
+		NodeGeneric cloneIterator = headClone;
+		while (ogIterator != null) {
+			NodeGeneric nodeClone = new NodeGeneric(null, null, ogIterator.get());
+			cloneIterator.next = nodeClone;
+			nodeClone.prev = cloneIterator;	
+			cloneIterator = cloneIterator.next;
+			ogIterator = ogIterator.next;
 		}
+		this.tail = cloneIterator;
 	}
 
 	//remove and return the item at the parameter's index
-	public void remove(int index) {
-
+	public NodeGeneric remove(int index) {
+		if (index == 1){
+			NodeGeneric headCopy =this.head;
+			this.head = this.head.next;
+			headCopy.next = null;
+			this.head.prev = null;
+			return this.head;
+		} 
+		else if (index == this.size()) {
+			NodeGeneric tailCopy = this.tail;
+			this.tail = this.tail.prev;
+			tailCopy.prev = null;
+			this.tail.next = null;
+			return this.tail;
+		}
+		else if (index>1 && index<this.size()) {
+			NodeGeneric currNode = this.head;
+			for(int currIndex = 0; currIndex < index; currIndex+=1) {
+				if (currIndex == index-1) {
+					currNode.prev.next = currNode.next;
+					currNode.next.prev = currNode.prev;
+					currNode.next = null;
+					currNode.prev = null;
+				}
+				else {
+					currNode = currNode.next;
+				}
+			}
+			return this.get(index);
+		} else {
+			return null;
+		}
 	}
 
 	//returns the index of the String parameter 
 	public int indexOf(String obj) {
+		NodeGeneric currNode = this.head;
+		int currNodeIndex = 0;
+		
+		while (currNodeIndex < this.size()) {
+			if (currNode.get() == obj) {
+				currNodeIndex += 1;
+				break;
+			}
+			currNode = currNode.next;
+			currNodeIndex += 1;
+		}
+		return currNodeIndex;
 	}
 	
 	//returns item at parameter's index
-	public void get(int index) {
+	public NodeGeneric get(int index) {
+		NodeGeneric currNode = this.head;
+		int currNodeIndex = 0;
+		int listSize = this.size();
+		
+		if (listSize >= index && listSize != 0) {
+			while (currNode != null && currNodeIndex < index-1) {
+			currNode = currNode.next;
+			currNodeIndex += 1;
+			}
+			return currNode;
+		} else {
+			return null;
+		}
 	}
 
 	//checks if there is a list
@@ -52,6 +113,13 @@ public class DSEListGeneric implements ListGeneric {
 
 	//return the size of the list
 	public int size() {
+		NodeGeneric currNode = this.head;
+		int size = 0;
+		while (currNode != null) {
+			currNode = currNode.next;
+			size+=1;
+		}
+		return size;
 	}
 	
 	//Take each element of the list a writes them to a string 
@@ -69,29 +137,71 @@ public class DSEListGeneric implements ListGeneric {
 
 	//add the parameter item at of the end of the list
 	public boolean add(Object obj) {
-	}
-
-	//add item at parameter's index
-	public boolean add(int index, Object obj) {
 		if (this.head == null) {
 			NodeGeneric addedNode = new NodeGeneric(null, null, obj);
 			this.head = addedNode;
 			this.tail = addedNode;
-			return false;
+			return true;
 		} else {
 			NodeGeneric addedNode = new NodeGeneric(null, this.tail, obj);
+			this.tail.next = addedNode;
 			this.tail = addedNode;
 			
+			return true;
+		}
+	}
+
+	//add item at parameter's index
+	public boolean add(int index, Object obj) {
+		if (index < this.size()+1 && index > 1) {
+			NodeGeneric currNode = this.head;
+			for(int currIndex = 0; currIndex < index-1; currIndex+=1) {
+				if (currIndex == index-2) {
+					NodeGeneric nodeMaker = new NodeGeneric(currNode.next, currNode, obj); 
+					currNode.next.prev = nodeMaker;
+					currNode.next = nodeMaker;
+					break;
+				}
+				currNode = currNode.next;
+			}	
+			return true;
+		}else if (index == 1) {
+			NodeGeneric headClone = this.head;
+			NodeGeneric nodeMaker = new NodeGeneric(this.head, null, obj); 
+			this.head = nodeMaker;
+			headClone.prev = nodeMaker;
+			return true;
+		}else if (index == this.size()+1) {
+			NodeGeneric tailClone = this.tail;
+			NodeGeneric nodeMaker = new NodeGeneric(null, this.tail, obj); 
+			this.tail = nodeMaker;
+			tailClone.next = nodeMaker;
+			return true;
+		}
+		else { 
 			return false;
 		}
 	}
 
 	//searches list for parameter's String return true if found
 	public boolean contains(Object obj) {
+		NodeGeneric currNode = this.head;
+		boolean isInList = false;
+		while (currNode.next != null || currNode.get() == obj) {
+			if (currNode.get() == obj) {
+				isInList = true;
+			}
+			if (currNode.next == null) {
+				break;
+			}
+			currNode = currNode.next;
+		}
+		return isInList;
 	}
 
 	//removes the parameter's item form the list
 	public boolean remove(Object obj) {
+		
 	}
 	
 	@Override
@@ -101,8 +211,6 @@ public class DSEListGeneric implements ListGeneric {
 
 	@Override
 	public boolean equals(Object other) {
-		Node LinkList1 = this.head;
-		//Node LinkList2 =;
 		return true;
 	}
 	@Override
